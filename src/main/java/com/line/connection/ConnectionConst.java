a6473351d950e25a4041311aaaea11b6ab100ffc
jinhyuck <gjwlsgur4866@gmail.com>
2022-10-18 13:06:06 +0900

    관심사 분리

--- src/main/java/com/line/connection/ConnectionConst.java
+++ src/main/java/com/line/connection/ConnectionConst.java
@@ -5,9 +5,9 @@ import java.util.Map;
 public class ConnectionConst {
 
     public static Map<String, String> env = System.getenv();
-    public static String dbHost = env.get("DB_HOST");
-    public static String dbUser = env.get("DB_USER");
-    public static String dbPassword = env.get("DB_PASSWORD");
+    public static final String dbHost = env.get("DB_HOST");
+    public static final String dbUser = env.get("DB_USER");
+    public static final String dbPassword = env.get("DB_PASSWORD");
 
 
 }
