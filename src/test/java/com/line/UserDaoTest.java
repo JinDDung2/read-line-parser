2e207694428dd9b0637c11cf825c07bb1b2c29b8
jinhyuck <gjwlsgur4866@gmail.com>
2022-10-18 14:34:36 +0900

    AWSUserDaoImpl 테스트

--- src/test/java/com/line/UserDaoTest.java
+++ src/test/java/com/line/UserDaoTest.java
@@ -2,7 +2,6 @@ package com.line;
 
 import com.line.dao.AWSUserDaoImpl;
 import com.line.dao.UserDao;
-import com.line.dao.UserDaoAbstract;
 import com.line.domain.User;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
@@ -13,9 +12,9 @@ class UserDaoTest {
 
     @Test
     void addAndSelect() {
-        UserDaoAbstract userDao = new AWSUserDaoImpl();
+        AWSUserDaoImpl userDao = new AWSUserDaoImpl();
         User user = new User("10", "ten", "1234");
-        userDao.add(user);
+//        userDao.add(user);
 
         User findUser = userDao.findById("10");
         Assertions.assertEquals("ten", findUser.getName());
