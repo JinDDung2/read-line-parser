2419364e50da56c148daf9eb829f33307f9de605
jinhyuck <gjwlsgur4866@gmail.com>
2022-10-18 14:50:58 +0900

    AWSUserDao 추상화

--- src/main/java/com/line/dao/AWSUserDaoImpl.java
+++ src/main/java/com/line/dao/AWSUserDaoImpl.java
@@ -3,11 +3,21 @@ package com.line.dao;
 import com.line.connection.DBConnection;
 
 import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+import static com.line.connection.ConnectionConst.*;
 
 public class AWSUserDaoImpl extends UserDaoAbstract{
     @Override
     public Connection makeConnection() {
-        Connection conn = DBConnection.getConnection();
-        return conn;
+        try {
+            Connection conn = DriverManager.getConnection(dbHost, dbUser, dbPassword);
+            return conn;
+
+        }catch (SQLException e) {
+            e.printStackTrace();
+            throw new IllegalStateException();
+        }
     }
 }
