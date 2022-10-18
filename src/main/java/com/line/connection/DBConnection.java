884c9fdbf1c7565ef7f0d90ee0cbc72041f33e19
jinhyuck <gjwlsgur4866@gmail.com>
2022-10-18 14:34:49 +0900

    connection 분리

--- src/main/java/com/line/connection/DBConnection.java
+++ src/main/java/com/line/connection/DBConnection.java
@@ -1,7 +1,31 @@
 package com.line.connection;
 
 import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 
-public interface DBConnection {
-    Connection getConnection();
+import static com.line.connection.ConnectionConst.*;
+
+
+public class DBConnection {
+
+    public DBConnection() {
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static Connection getConnection() {
+        try {
+            Connection conn = DriverManager.getConnection(dbHost, dbUser, dbPassword);
+            return conn;
+
+        }catch (SQLException e) {
+            e.printStackTrace();
+            throw new IllegalStateException();
+        }
+    }
+
 }
