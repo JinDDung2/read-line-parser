3f31a3edce2b66662164a056f7964c81b117d602
jinhyuck <gjwlsgur4866@gmail.com>
2022-10-18 13:32:32 +0900

    UserDaoTest 리팩토링

--- src/test/java/com/line/UserDaoTest.java
+++ src/test/java/com/line/UserDaoTest.java
@@ -1,5 +1,8 @@
 package com.line;
 
+import com.line.dao.AWSUserDaoImpl;
+import com.line.dao.UserDao;
+import com.line.dao.UserDaoAbstract;
 import com.line.domain.User;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
@@ -10,11 +13,11 @@ class UserDaoTest {
 
     @Test
     void addAndSelect() {
-        UserDao userDao = new UserDao();
-        User user = new User("8", "EternityHwan", "1234");
+        UserDaoAbstract userDao = new AWSUserDaoImpl();
+        User user = new User("9", "nine", "1234");
         userDao.add(user);
 
-        User findUser = userDao.findById("8");
-        Assertions.assertEquals("EternityHwan", findUser.getName());
+        User findUser = userDao.findById("9");
+        Assertions.assertEquals("nine", findUser.getName());
     }
 }
