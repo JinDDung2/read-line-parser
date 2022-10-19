e55b8b85fb12dae4343fd59eef1fd3a26e90e200
jinhyuck <gjwlsgur4866@gmail.com>
2022-10-19 11:48:47 +0900

    factory 적용 테스트

--- src/test/java/com/line/UserDaoTest.java
+++ src/test/java/com/line/UserDaoTest.java
@@ -2,6 +2,7 @@ package com.line;
 
 import com.line.dao.AWSUserDaoImpl;
 import com.line.dao.UserDao;
+import com.line.dao.UserDaoFactory;
 import com.line.domain.User;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
@@ -12,11 +13,11 @@ class UserDaoTest {
 
     @Test
     void addAndSelect() {
-        UserDao userDao = new UserDao();
-        User user = new User("11", "IVE", "1234");
+        UserDao userDao = new UserDaoFactory().awsUserDao();
+        User user = new User("12", "twelve", "1234");
         userDao.add(user);
 
-        User findUser = userDao.findById("11");
-        Assertions.assertEquals("IVE", findUser.getName());
+        User findUser = userDao.findById("12");
+        Assertions.assertEquals("twelve", findUser.getName());
     }
 }
