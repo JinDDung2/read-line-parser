1077128bbd9ec4d09e81f715888384c8d37d37b7
jinhyuck <gjwlsgur4866@gmail.com>
2022-10-20 12:51:23 +0900

    DBConnection 리팩토링

---
+++ src/main/java/com/likelion/connection/DBConnection.java
@@ -0,0 +1,31 @@
+package com.likelion.connection;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+import static com.likelion.connection.ConnectionConst.*;
+
+
+public class DBConnection {
+
+    public DBConnection() {
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public Connection getConnection() {
+        try {
+            Connection conn = DriverManager.getConnection(dbHost, dbUser, dbPassword);
+            return conn;
+
+        }catch (SQLException e) {
+            e.printStackTrace();
+            throw new IllegalStateException();
+        }
+    }
+
+}
