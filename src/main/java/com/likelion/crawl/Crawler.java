715d55d2059d5182eb81d93bd5dc372b6a0aa7a3
jinhyuck <gjwlsgur4866@gmail.com>
2022-10-20 12:50:57 +0900

    셔울 병원 데이터 가공

---
+++ src/main/java/com/likelion/crawl/Crawler.java
@@ -0,0 +1,9 @@
+package com.likelion.crawl;
+
+import java.net.http.HttpClient;
+
+public class Crawler {
+    public static void main(String[] args) {
+        HttpClient hc = HttpClient.newHttpClient();
+    }
+}
