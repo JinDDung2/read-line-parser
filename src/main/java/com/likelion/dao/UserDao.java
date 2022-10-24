2af95f23ab12e2da10a65135fd0d4e07afe667dd
jinhyuck <gjwlsgur4866@gmail.com>
2022-10-24 15:13:00 +0900

    JdbcTemplate 적용

--- src/main/java/com/likelion/dao/UserDao.java
+++ src/main/java/com/likelion/dao/UserDao.java
@@ -1,6 +1,8 @@
 package com.likelion.dao;
 
 import com.likelion.domain.User;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
 
 import javax.sql.DataSource;
 import java.sql.Connection;
@@ -10,98 +12,38 @@ import java.sql.SQLException;
 
 public class UserDao {
 
-    private final DataSource dataSource;
-    private final JdbcContext jdbcContext;
+    private final JdbcTemplate template;
 
     public UserDao(DataSource dataSource) {
-        this.dataSource = dataSource;
-        this.jdbcContext = new JdbcContext(dataSource);
+        this.template = new JdbcTemplate(dataSource);
     }
 
     public void save(User user) {
-        jdbcContext.workWithStatementStrategy(
-                new StatementStrategy() {
-                    @Override
-                    public PreparedStatement makePreparedStatement(Connection conn) throws SQLException {
-                        String sql = "INSERT INTO users(id, name, password) values(?, ?, ?)";
-                        PreparedStatement ps = conn.prepareStatement(sql);
-                        ps.setString(1, user.getId());
-                        ps.setString(2, user.getName());
-                        ps.setString(3, user.getPassword());
-                        return ps;
-                    }
-                }
-        );
+        String sql = "INSERT INTO users(id, name, password) VALUES(?, ?, ?)";
+        template.update(sql, user.getId(), user.getName(), user.getPassword());
     }
 
     public User findById(String id) {
         String sql = "select * from users where users.id=?";
-        Connection conn = null;
-        PreparedStatement ps = null;
-        ResultSet rs = null;
+        return template.queryForObject(sql, userRowMapper(), id);
+    }
 
-        try {
-            conn = dataSource.getConnection();
-            ps = conn.prepareStatement(sql);
-            ps.setString(1, id);
-            rs = ps.executeQuery();
-
-            if (rs.next()) {
-                User user = new User(rs.getString("id"),
-                        rs.getString("name"), rs.getString("password"));
-                return user;
-            } else {
-                throw new RuntimeException();
-            }
-        } catch (SQLException e) {
-            throw new RuntimeException();
-        } finally {
-            close(conn, ps, rs);
-        }
+    private RowMapper<User> userRowMapper() {
+        return ((rs, rowNum) -> {
+            User user = new User(rs.getString("id"),
+                    rs.getString("name"), rs.getString("password"));
+            return user;
+        });
     }
 
     public void deleteAll() {
-        jdbcContext.workWithStatementStrategy(new StatementStrategy() {
-            @Override
-            public PreparedStatement makePreparedStatement(Connection conn) throws SQLException {
-                String sql = "DELETE FROM users";
-                PreparedStatement ps = conn.prepareStatement(sql);
-                return ps;
-            }
-        });
+        String sql = "DELETE FROM users";
+        template.update(sql);
     }
 
     public int getCount(){
         String sql = "SELECT count(*) FROM users";
-        Connection conn = null;
-        PreparedStatement ps = null;
-        ResultSet rs = null;
-
-        try {
-            conn = dataSource.getConnection();
-            ps = conn.prepareStatement(sql);
-            rs = ps.executeQuery();
-            rs.next();
-            return rs.getInt(1);
-        } catch (SQLException e) {
-            e.printStackTrace();
-            throw new RuntimeException();
-        } finally {
-            close(conn, ps, rs);
-        }
-    }
-
-    // 가변인자로 받아서, 파라미터를 여러개 받는다.
-    private void close(AutoCloseable... autoCloseable) {
-        for (AutoCloseable ac : autoCloseable) {
-            if (ac != null) {
-                try {
-                    ac.close();
-                } catch (Exception e) {
-                    throw new RuntimeException(e);
-                }
-            }
-        }
+        return template.queryForObject(sql, Integer.class);
     }
 
 }
