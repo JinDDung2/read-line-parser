eab52401c5e46017049c27b30303a89a48ce1a56
jinhyuck <gjwlsgur4866@gmail.com>
2022-10-24 15:50:04 +0900

    JdbcTemplate 적용

--- src/main/java/com/likelion/dao/UserDao.java
+++ src/main/java/com/likelion/dao/UserDao.java
@@ -2,14 +2,9 @@ package com.likelion.dao;
 
 import com.likelion.domain.User;
 import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.jdbc.core.RowCallbackHandler;
 import org.springframework.jdbc.core.RowMapper;
 
 import javax.sql.DataSource;
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
 import java.util.List;
 
 public class UserDao {
